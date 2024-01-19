0face6e945ccacc04b317a4c34b461f6d3701851
roby238 <roby238_dev@naver.com>
2024-01-19 09:43:14 +0100

    feat: Forge multi-server linked client

--- ccu_socket/ccu_client.c
+++ ccu_socket/ccu_client.c
@@ -15,8 +15,10 @@
 #define DATA_SIZE 1024
 
 void integral(int mode, char target, int pos);
-void * send_msg(void * arg);
-void * recv_msg(void * arg);
+void * ccu_send_msg(void * arg);
+void * ccu_recv_msg(void * arg);
+void * platoon_send_msg(void * arg);
+void * platoon_recv_msg(void * arg);
 void * timer_msg(void * arg);
 void error_handling(char * msg);
 
@@ -41,9 +43,9 @@ typedef struct {
 	double pos_y[DATA_SIZE];
 } CCU_BUF_T;
 
-pthread_mutex_t gMutex;
-char name[NAME_SIZE]="[Default]";
-char msg[BUF_SIZE];
+pthread_mutex_t g_mutex[2];
+char name[2][NAME_SIZE]="[Default]";
+char msg[2][BUF_SIZE];
 PACU_BUF_T pacu_buf[2];
 CCU_BUF_T ccu_buf[2];
 int pacu_pos[2];
@@ -52,43 +54,61 @@ int pacu_pos[2];
 struct timeval prev_time, curr_time;
 
 int main(int argc, char *argv[]) {
-	int sock;
-	struct sockaddr_in serv_addr;
-	pthread_t snd_thread, rcv_thread, timer_thread;
+	int sock[2];
+	struct sockaddr_in ccu_serv_addr;
+	struct sockaddr_in platoon_serv_addr;
+	pthread_t ccu_snd_thread, ccu_rcv_thread, timer_thread;
 	void * thread_return;
 
-	if(argc != 4) {
-		printf("Usage : %s <IP> <port> <name>\n",argv[0]);
+	if(argc != 7) {
+		printf("Usage : %s <CCU IP> <port> <name> <PLATOON IP> <port> <name>\n",argv[0]);
 		exit(1);
 	}
-	sprintf(name, "%s",argv[3]);
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if(sock == -1) {
+	sock[0] = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock[0] == -1) {
 		error_handling("socket() error");
 	}
 	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-	serv_addr.sin_port = htons(atoi(argv[2]));
+	
+	ccu_serv_addr.sin_family=AF_INET;
+	ccu_serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
+	ccu_serv_addr.sin_port = htons(atoi(argv[2]));
 
-	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+	platoon_serv_addr.sin_family=AF_INET;
+	platoon_serv_addr.sin_addr.s_addr = inet_addr(argv[4]);
+	platoon_serv_addr.sin_port = htons(atoi(argv[5]));
+	
+	if(connect(sock[0], (struct sockaddr *)&ccu_serv_addr, sizeof(ccu_serv_addr)) == -1) {
+		error_handling("connect() error");
+	}
+	if(connect(sock[1], (struct sockaddr *)&platoon_serv_addr, sizeof(platoon_serv_addr)) == -1) {
 		error_handling("connect() error");
 	}
 
+	sprintf(name, "%s",argv[3]);
 	sprintf(msg,"[%s:PASSWD]",name);
-	write(sock, msg, strlen(msg));
-	pthread_create(&rcv_thread, NULL, recv_msg, (void *)&sock);
-	pthread_create(&snd_thread, NULL, send_msg, (void *)&sock);
-	pthread_create(&timer_thread, NULL, timer_msg, (void *)&sock);
+	write(sock[0], msg, strlen(msg));
+	
+	sprintf(name, "%s",argv[6]);
+	sprintf(msg,"[%s:PASSWD]",name);
+	write(sock[1], msg, strlen(msg));
 
-	pthread_join(snd_thread, &thread_return);
+	pthread_create(&ccu_rcv_thread, NULL, ccu_recv_msg, (void *)&sock[0]);
+	pthread_create(&ccu_snd_thread, NULL, ccu_send_msg, (void *)&sock[0]);
+	pthread_create(&platoon_rcv_thread, NULL, platoon_recv_msg, (void *)&sock[1]);
+	pthread_create(&platoon_snd_thread, NULL, platoon_send_msg, (void *)&sock[1]);
+	pthread_create(&timer_thread, NULL, timer_msg, (void *)&sock[1]);
+
+	pthread_join(ccu_snd_thread, &thread_return);
+	pthread_join(platoon_snd_thread, &thread_return);
 	//pthread_join(rcv_thread, &thread_return);
 	//pthread_join(timer_thread, &thread_return);
-	close(sock);
+	close(sock[0]);
+	close(sock[1]);
 	return 0;
 }
 
-void integral(int mode, char target, int pos) {
+void set_integral(int mode, char target, int pos) {
 	if (!pos) return;
 	int interval = ((pacu_buf[mode].curr_time[pos].tv_usec - pacu_buf[mode].curr_time[pos-1].tv_usec))/1000000000;
 	if (target == 'a') {
@@ -102,7 +122,7 @@ void integral(int mode, char target, int pos) {
 	}	
 }
 
-void * send_msg(void * arg) {
+void * ccu_send_msg(void * arg) {
 	int *sock = (int *)arg;
 	int str_len;
 	int ret;
@@ -122,7 +142,7 @@ void * send_msg(void * arg) {
 		newset = initset;
 		ret = select(STDIN_FILENO + 1, &newset, NULL, NULL, &tv);
 		if(FD_ISSET(STDIN_FILENO, &newset)) {
-			pthread_mutex_lock(&gMutex);
+			pthread_mutex_lock(&g_mutex[0]);
 			fgets(msg, BUF_SIZE, stdin);
 			if(!strncmp(msg,"quit\n",5)) {
 				*sock = -1;
@@ -137,7 +157,7 @@ void * send_msg(void * arg) {
 				*sock = -1;
 				return NULL;
 			}
-			pthread_mutex_unlock(&gMutex);
+			pthread_mutex_unlock(&g_mutex[0]);
 		}
 		if(ret == 0) {
 			if(*sock == -1) return NULL;
@@ -145,7 +165,7 @@ void * send_msg(void * arg) {
 	}
 }
 
-void * recv_msg(void * arg) {
+void * ccu_recv_msg(void * arg) {
 	int * sock = (int *)arg;	
 	int i;
 	char *pToken;
@@ -154,7 +174,7 @@ void * recv_msg(void * arg) {
 	int str_len;
 
 	while(1) {
-		pthread_mutex_lock(&gMutex);
+		pthread_mutex_lock(&g_mutex[0]);
 		memset(name_msg,0x0,sizeof(name_msg));
 		str_len = read(*sock, name_msg, NAME_SIZE + BUF_SIZE );
 		if(str_len <= 0) {
@@ -176,10 +196,9 @@ void * recv_msg(void * arg) {
 			sscanf(pArray[4], "%lf", &(pacu_buf[0].acc_y[pacu_pos[0]]));	
 			sscanf(pArray[5], "%lf", &(pacu_buf[0].gyro[pacu_pos[0]]));	
 			sscanf(pArray[6], "%lf", &(pacu_buf[0].angle[pacu_pos[0]]));
-			//clock_gettime(CLOCK_MONOTONIC, &(pacu_buf[0].curr_time[pacu_pos[0]]));
 			gettimeofday(&(pacu_buf[0].curr_time[pacu_pos[0]]), NULL);
-			integral(0, 'a', pacu_pos[0]);
-			integral(0, 'd', pacu_pos[0]);
+			set_integral(0, 'a', pacu_pos[0]);
+			set_integral(0, 'd', pacu_pos[0]);
 			pacu_pos[0]++;
 			if (pacu_pos[0] >= DATA_SIZE) pacu_pos[0] = 0;
 		}
@@ -189,14 +208,94 @@ void * recv_msg(void * arg) {
 			sscanf(pArray[4], "%lf", &(pacu_buf[1].acc_y[pacu_pos[1]]));	
 			sscanf(pArray[5], "%lf", &(pacu_buf[1].gyro[pacu_pos[1]]));	
 			sscanf(pArray[6], "%lf", &(pacu_buf[1].angle[pacu_pos[1]]));	
-			//clock_gettime(CLOCK_MONOTONIC, &(pacu_buf[1].curr_time[pacu_pos[1]]));
 			gettimeofday(&(pacu_buf[0].curr_time[pacu_pos[0]]), NULL);
-			integral(1, 'a', pacu_pos[1]);
-			integral(1, 'd', pacu_pos[1]);
+			set_integral(1, 'a', pacu_pos[1]);
+			set_integral(1, 'd', pacu_pos[1]);
 			pacu_pos[1]++;
 			if (pacu_pos[1] >= DATA_SIZE) pacu_pos[1] = 0;
 		}
-		pthread_mutex_unlock(&gMutex);
+		pthread_mutex_unlock(&g_mutex[0]);
+
+	}
+}
+
+void * platoon_send_msg(void * arg) {
+	int *sock = (int *)arg;
+	int str_len;
+	int ret;
+	fd_set initset, newset;
+	struct timeval tv;
+	char name_msg[NAME_SIZE + BUF_SIZE+2];
+
+	FD_ZERO(&initset);
+	FD_SET(STDIN_FILENO, &initset);
+
+	fputs("Input a message! [ID]msg (Default ID:ALLMSG)\n",stdout);
+	while(1) {
+		memset(msg,0,sizeof(msg));
+		name_msg[0] = '\0';
+		tv.tv_sec = 1;
+		tv.tv_usec = 0;
+		newset = initset;
+		ret = select(STDIN_FILENO + 1, &newset, NULL, NULL, &tv);
+		if(FD_ISSET(STDIN_FILENO, &newset)) {
+			pthread_mutex_lock(&g_mutex[1]);
+			fgets(msg, BUF_SIZE, stdin);
+			if(!strncmp(msg,"quit\n",5)) {
+				*sock = -1;
+				return NULL;
+			}
+			else if(msg[0] != '[') {
+				strcat(name_msg,"[ALLMSG]");
+				strcat(name_msg,msg);
+			}
+			else strcpy(name_msg,msg);
+			if(write(*sock, name_msg, strlen(name_msg))<=0) {
+				*sock = -1;
+				return NULL;
+			}
+			pthread_mutex_unlock(&g_mutex[1]);
+		}
+		if(ret == 0) {
+			if(*sock == -1) return NULL;
+		}
+	}
+}
+
+void * platoon_recv_msg(void * arg) {
+	int * sock = (int *)arg;	
+	int i;
+	char *pToken;
+	char *pArray[ARR_CNT]={0};
+	char name_msg[NAME_SIZE + BUF_SIZE +1];
+	int str_len;
+
+	while(1) {
+		pthread_mutex_lock(&g_mutex[1]);
+		memset(name_msg,0x0,sizeof(name_msg));
+		str_len = read(*sock, name_msg, NAME_SIZE + BUF_SIZE );
+		if(str_len <= 0) {
+			*sock = -1;
+			return NULL;
+		}
+		name_msg[str_len] = 0;
+		fputs(name_msg, stdout);
+		pToken = strtok(name_msg, "[:@]");
+		i = 0;
+		while (pToken != NULL) {
+			pArray[i] = pToken;
+			if ( ++i >= ARR_CNT) break;
+			pToken = strtok(NULL, "[:@]");
+		}
+		if (!strcmp(pArray[1],"PLATOON")) {
+		}
+		else if (!strcmp(pArray[1], "LEADER")) {
+		}
+		else if (!strcmp(pArray[1], "FOLLOWER1")) {
+		}
+		else if (!strcmp(pArray[1], "FOLLOWER2")) {
+		}
+		pthread_mutex_unlock(&g_mutex[1]);
 
 	}
 }
@@ -205,19 +304,19 @@ void * timer_msg(void * arg) {
 	int *sock = (int *)arg;
 	char name_msg[NAME_SIZE + BUF_SIZE +1];
 	gettimeofday(&prev_time, NULL);
-	//gettimeofday(&curr_time, NULL);
 	double global_interval;
 	while(1) {
-		//clock_gettime(CLOCK_MONOTONIC, &(curr_time));
 		gettimeofday(&curr_time, NULL);
 		global_interval = ((double)(curr_time.tv_sec - prev_time.tv_sec) * 1000 + (double)(curr_time.tv_usec - prev_time.tv_usec)/1000);
-		//if (curr_time.tv_nsec < prev_time.tv_usec) global_interval = curr_time.tv_usec - prev_time.tv_usec;
 		if (global_interval >= 100) {
 			//pthread_mutex_lock(&gMutex);
-			memset(msg,0,sizeof(msg));
-        		name_msg[0] = '\0';
 			
-			sprintf(msg, "[GUI]DATA@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lld@%ld\n", 
+        		for (int i = 0; i < 2; i++) {
+				memset(&msg[i],0,sizeof(&msg[i]));
+			}
+			name_msg[0] = '\0';
+			
+			sprintf(&msg[1], "[GUI]DATA@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lf@%lld@%ld\n", 
 					pacu_buf[0].dist[pacu_pos[0]], pacu_buf[1].dist[pacu_pos[1]], 
 					(pacu_buf[0].acc_x[pacu_pos[0]] + pacu_buf[1].acc_x[pacu_pos[1]]) / 2, 
 					(pacu_buf[0].acc_y[pacu_pos[0]] + pacu_buf[1].acc_y[pacu_pos[1]]) / 2, 
@@ -229,9 +328,8 @@ void * timer_msg(void * arg) {
 					(pacu_buf[0].gyro[pacu_pos[0]] + pacu_buf[1].gyro[pacu_pos[1]]) / 2,
 					(long long)(pacu_buf[0].curr_time[pacu_pos[0]].tv_sec + pacu_buf[1].curr_time[pacu_pos[1]].tv_sec) * 500 + (long long)(pacu_buf[0].curr_time[pacu_pos[0]].tv_usec + pacu_buf[1].curr_time[pacu_pos[1]].tv_usec) * 500);
 			
-			//sprintf(msg, "[GUI]DATA@sdfgghhjk\n");
-			strcmp(name_msg, msg);	 
-			write(*sock, msg, strlen(msg));
+			//strcmp(name_msg, msg);	 
+			write(*sock, &msg[1], strlen(&msg[1]));
 			
 			//pthread_mutex_unlock(&gMutex);
 			prev_time.tv_sec = curr_time.tv_sec;
@@ -239,7 +337,6 @@ void * timer_msg(void * arg) {
 			//printf("%ld\n", curr_time.tv_usec);	
 			//printf("%lf\n", (double)curr_time.tv_sec + (double)curr_time.tv_usec / 1000000);	
 		}
-		
 	}
 }
 
